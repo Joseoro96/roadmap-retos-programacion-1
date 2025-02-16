00853eb4b65128ee3dc7f8a00549b1204a022f7e
Joseoro96 <164701721+Joseoro96@users.noreply.github.com>
2025-02-15 19:16:05 -0600

    ose_Orozco.py
    Resolviendo el Ejercicio_1 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jose_Orozco.py
@@ -0,0 +1,32 @@
+print("Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.")
+
+#Python, https://www.python.org/
+
+print("Comentario")
+# Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+# Este es un comentario de una sola linea, utilizando #.
+
+"""
+Este es un 
+comentario
+de multiples 
+lineas
+utilizando \"""
+
+"""
+
+print("Crea una variable (y una constante si el lenguaje lo soporta).")
+
+variable = "con letras minusculas es una variable"
+CONSTANTE = "CON LETRAS MAYUSCULAS ES UNA CONSTANTE"
+
+print("Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).")
+
+Texto = "Cadena de texto"
+Entero = 12
+Flotante = 14.5
+Booleano = True, False
+
+# Imprime por terminal el texto: ¡Hola, [y el nombre de tu lenguaje]!")
+
+print("¡Hola, Python!")
